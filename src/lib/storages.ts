9b72e8b47610795e3391caa6a3df6c641e70d661
Yan Roger Fogaça Vieira <yanrogerfv@gmail.com>
2025-05-08 23:23:56 -0300

    refac: changed approach in storage.ts

--- src/lib/storages.ts
+++ src/lib/storages.ts
@@ -1,6 +1,32 @@
-// "use client"
+"use client"
 
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+
+export default function useSessionStorage(key: string, initialValue: any) {
+    const [storedValue, setStoredValue] = useState(() => {{
+        try {
+            const item = sessionStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        }
+        catch (error) {
+            console.error(`Error reading sessionStorage key “${key}”:`, error);
+            return initialValue;
+        }
+    }});
+
+    const setValue = (value: any) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            sessionStorage.setItem(key, JSON.stringify(valueToStore));
+        }
+        catch (error) {
+            console.error(`Error setting sessionStorage key “${key}”:`, error);
+        }
+    };
+
+    return [storedValue, setValue];
+}
 
 // const [session, setSession] = useState(false);
 
