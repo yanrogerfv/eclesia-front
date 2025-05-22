d629365490b5a3091dab0aaa130ad50fd91c9d4f
yan-roger-fv <yanrogerfv@gmail.com>
2025-05-22 15:32:34 -0300

    fix: removed inexistent dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import ThemeSelector from "@/components/themeSelector";
-import { ToastSimple } from "@/components/toastSimple";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
